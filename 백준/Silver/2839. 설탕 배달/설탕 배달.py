7baa7999f968c473c03cc1d35ccd86a1633b0274
박가희 <153144067+godheezzang@users.noreply.github.com>
2024-09-03 15:20:40 +0900

    [Silver IV] Title: 설탕 배달, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,20 @@
+
+n = int(input())
+
+if n % 5 == 0:
+    print(n//5)
+else:
+    cnt = 0
+    while n > 0:
+        n -= 3
+        cnt += 1
+        # 5의 배수 + 3의 배수
+        if n % 5 == 0:
+            cnt += n // 5
+            print(cnt)
+            break
+        elif n == 1 or n == 2:
+            print(-1)
+            break
+        elif n == 0:
+            print(cnt)
